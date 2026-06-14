1b2e93ea839700fe9fc68879a96bca2c121264b9
MugandaJames <mugandajames403@gmail.com>
2026-06-14 16:47:52 +0300

    clean initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JiSaveSacco.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // We will map tables here later
+        // public DbSet<Member> Members { get; set; }
+        // public DbSet<User> Users { get; set; }
+        // public DbSet<SavingsAccount> SavingsAccounts { get; set; }
+        // public DbSet<SavingsTransaction> SavingsTransactions { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+using JiSaveSacco.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database connection (MySQL)
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 0))
+    );
+});
+
+var app = builder.Build();
+
+// Swagger UI
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
